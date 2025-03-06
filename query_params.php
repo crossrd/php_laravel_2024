23520c8351d78a2aae2f31e365a4334da1b23460
Ekkamon Thiansimuang <ekkamon.gta@gmail.com>
2025-03-06 12:48:38 +0700

    first day of php course

---
+++ query_params.php
@@ -0,0 +1,6 @@
+<h3>Query Params</h3>
+<?php
+if (isset($_GET['name'])) { // name=eak
+    echo $_GET['name'];
+}
+?>
---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "John";
+$age = 20;
+
+echo $name;
+echo $age;
+
+?>
